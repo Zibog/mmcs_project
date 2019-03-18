cd59b83f281afe558e13a96be73ff1c36b6e005b
Damir <34239251+drKazeev@users.noreply.github.com>
2019-03-18 12:30:23 +0400

    alpha version

--- src/main/java/com/example/main/MainActivity.java
+++ src/main/java/com/example/main/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.main;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bv = (Button)findViewById(R.id.button);
+        Typeface face = Typeface.createFromAsset(this.getAssets(), "fonts/13059.otf");
+        bv.setTypeface(face);
+        Button bv2 = (Button)findViewById(R.id.button2);
+        bv2.setTypeface(face);
+        Button bv3 = (Button)findViewById(R.id.button3);
+        bv3.setTypeface(face);
+        Button bv4 = (Button)findViewById(R.id.button4);
+        bv4.setTypeface(face);
     }
 }
+

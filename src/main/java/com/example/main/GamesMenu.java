98e72bb5df302a31314772a62e854c920a5a5ddf
Damir <34239251+drKazeev@users.noreply.github.com>
2019-04-14 20:02:14 +0400

    fix fonts in game menu

--- src/main/java/com/example/main/GamesMenu.java
+++ src/main/java/com/example/main/GamesMenu.java
@@ -1,9 +1,11 @@
 package com.example.main;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class GamesMenu extends AppCompatActivity {
 
@@ -14,6 +16,17 @@ public class GamesMenu extends AppCompatActivity {
         findViewById(R.id.button).setEnabled(false);
         findViewById(R.id.button3).setEnabled(false);
         findViewById(R.id.button4).setEnabled(false);
+
+        Typeface face = Typeface.createFromAsset(this.getAssets(), "fonts/13059.otf");
+        Button bv = (Button)findViewById(R.id.button);
+        bv.setTypeface(face);
+
+        Button bv2 = (Button)findViewById(R.id.button3);
+        bv2.setTypeface(face);
+
+        Button bv3 = (Button)findViewById(R.id.button2);
+        bv3.setTypeface(face);
+
     }
 
     public void MosaicOnClick(View view) {
